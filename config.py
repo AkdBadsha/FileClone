56899e4dda75d796c7286a4e76ee6cd9b1eb070e
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2024-01-27 22:47:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://cloneweb.onrender.com"
+        URL = "https://cloneweb.onrender.com/"
     else:
-        URL = "https://cloneweb.onrender.com"
+        URL = "https://cloneweb.onrender.com/"
 
 
 
